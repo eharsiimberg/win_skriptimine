dcc13c9ac1a46798af90ee27110e2b38cf8c2321
Ehar Siimberg <ehar.siimberg@voco.ee>
2022-11-16 17:01:55 +0200

    Ülesanne 1

---
+++ ülesanne1.ps1
@@ -0,0 +1,17 @@
+﻿echo "Siseta oma eesnimi:"
+$Eesnimi = Read-Host
+echo "Sisesta oma perenimi:"
+$Perenimi = Read-Host
+echo "Luuakse kasutaja $Eesnimi.$Perenimi"´#$KasutajaParool = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
+#
+$KasutajaNimi = "$Eesnimi" + "." + "$Perenimi"
+$TaisNimi = "$Eesnimi" + " " + "$Perenimi"
+#
+$ErrorActionPreference = "SilentlyContinue"
+#
+New-LocalUser $KasutajaNimi -Password $KasutajaParool -FullName "$TaisNimi"
+if (!$?)
+{
+echo "Tekkis probleem kasutaja loomisega"
+}
+$ErrorActionPreference = "Stop"
