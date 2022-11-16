f95468089d0fde0c0df3d1574112feacccdde84b
Ehar Siimberg <ehar.siimberg@voco.ee>
2022-11-16 16:44:09 +0200

    Praktikum1 ülesanded

---
+++ kasutaja_loomine_1.ps1
@@ -0,0 +1,13 @@
+﻿# Script start
+if($args.Count -ne 3){
+    echo '.\SkriptiNimi kasutajanimi "eesnimi perenimi" "Konto kirjeldus"'}
+else {
+    $KasutajaNimi = $args[0]
+    $TaisNimi = $args[1]
+    $KontoKirjeldus = $args[2]
+    # Parool
+    $KasutajaParool = ConvertTo-SecureString "Qwerty!" -AsPlainText -Force
+    # Kasutaja loomine andmetega
+    New-LocalUser "$KasutajaNimi" -Password $KasutajaParool -FullName "$TaisNimi" -Description "$KontoKirjeldus"
+}
+# End of script
---
+++ kasutaja_loomine_2.ps1
@@ -0,0 +1,14 @@
+﻿# Fail kus on andmed
+$Fail = "C:\Users\Administrator\Documents\kasutajad.csv"
+# Loeme faili sisu
+$Kasutajad = Import-Csv $Fail -Encoding Default -Delimiter ";"
+#
+foreach ($kasutaja in $kasutajad)
+{
+    $KasutajaNimi = $kasutaja.Kasutajanimi
+    $Taisnimi = $kasutaja.Taisnimi
+    $KontoKirjeldus = $kasutaja.KontoKirjeldus
+    $Parool = $kasutaja.Parool | ConvertTo-SecureString -AsPlainText -Force
+    #
+    New-LocalUser -Name $KasutajaNimi -Password $Parool -FullName "$Taisnimi" -Description "$KontoKirjeldus"
+}
