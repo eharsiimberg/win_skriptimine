1d6b01e49d6b703527bd39d6a04ebf854a07cb6b
Ehar Siimberg <ehar.siimberg@voco.ee>
2022-11-21 10:16:25 +0200

    Ülesanne 2. Konto kustutamine

---
+++ ülesanne2.ps1
@@ -0,0 +1,19 @@
+﻿echo "Siseta oma eesnimi:"
+$Eesnimi = Read-Host
+echo "Sisesta oma perenimi:"
+$Perenimi = Read-Host
+echo "Toimub kasutaja $Eesnimi.$Perenimi kustutamine"´#
+$KasutajaNimi = "$Eesnimi" + "." + "$Perenimi"
+#
+$ErrorActionPreference = "SilentlyContinue"
+#
+Remove-LocalUser $KasutajaNimi
+if (!$?)
+{
+echo "Tekkis probleem kasutaja loomisega"
+}
+else
+{
+echo "Konto $Kasutajanimi on kustutatud!"
+}
+$ErrorActionPreference = "Stop"
