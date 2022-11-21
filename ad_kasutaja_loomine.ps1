611e9d26b5c7e49aac671abc3434ef7637b5af89
Ehar Siimberg <ehar.siimberg@voco.ee>
2022-11-21 11:16:48 +0200

    Powershelliga AD Ksutajate loomine .csv failist

---
+++ ad_kasutaja_loomine.ps1
@@ -0,0 +1,47 @@
+﻿# Kasutajate Fail
+$fail = "C:\Users\Administrator\Documents\skriptimine\adkasutajad.csv"
+# Importimine
+$kasutajad = Import-Csv $fail -Encoding Default -Delimiter ";"
+#
+foreach ($kasutaja in $kasutajad){
+    $kasutajanimi = $kasutaja.FirstName + "." + $kasutaja.LastName
+    $kasutajanimi = $kasutajanimi.ToLower()
+    $kasutajanimi = TransLit($kasutajanimi)
+    #
+    $upname = $kasutajanimi + "@sv-kool.local"
+    #
+    $displayname = $kasutaja.FirstName + " " + $kasutaja.LastName
+    New-ADUser -Name $kasutajanimi `
+        -DisplayName $displayname `
+        -GivenName $kasutaja.FirstName `
+        -Surname $kasutaja.LastName `
+        -Department $kasutaja.Department `
+        -Title $kasutaja.Role `
+        -UserPrincipalName $upname `
+        -AccountPassword (ConvertTo-SecureString $kasutaja.Password -AsPlainText -Force) -Enabled $true
+}
+# Täpitähe fix
+function Translit {
+    param(
+        [string] $inputString
+    )
+    #
+        $TransLit = @{
+        [char]'ä' = "a"
+        [char]'ö' = "o"
+        [char]'ü' = "u"
+        [char]'õ' = "o"
+        }
+    #
+    $outputString=""
+    #
+    foreach ($character in $inputCharacter = $inputString.ToCharArray())
+    {
+        if ($TransLit[$character] -cne $Null ){
+            $outputString += $TransLit[$character]
+        } else {
+            $outputString += $character
+        }
+    }
+    write-Output $outputString
+}
